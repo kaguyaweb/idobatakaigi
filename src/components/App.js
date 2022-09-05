cd27ca0f628886ce1cb3097a8a6df04fe0a87f05
kaguyaweb <h1810518@edu.cc.uec.ac.jp>
2022-09-05 17:39:16 +0900

    ログイン画面をjsonで表示するか管理

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 
 import Main from './Main';
 import SignIn from './SignIn';
+import config from '../config.json';
 
 export default () => {
   // [状態, 関数]
   const [name, setName] = useState('');
-  console.log({ name });
 
-  if(name === '') {
+  if(config.signInEnabled && name === '') {
     return <SignIn setName={setName} />;
   } else  {
     return <Main name={name} />;
