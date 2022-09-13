4ebea67106a593b1942b92c0f653ab1d17656bf0
kaguyaweb <h1810518@edu.cc.uec.ac.jp>
2022-09-13 17:57:00 +0900

    set grid item

--- src/firebase.js
+++ src/firebase.js
@@ -11,8 +11,8 @@ appId: "1:254552960959:web:6a4ade20533d351ab93b0a"
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-const messageRef = database.ref('messages');
+const messagesRef = database.ref('messages');
 
 export const pushMessage = ({ name, text }) => {
-    messageRef.push({ name, text });
+    messagesRef.push({ name, text });
 }
