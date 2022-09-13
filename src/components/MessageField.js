7105297a80a54fb62433daceb64ed70275b57e9d
kaguyaweb <h1810518@edu.cc.uec.ac.jp>
2022-09-13 17:52:15 +0900

    create app

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { TextField } from "@material-ui/core";
 
+import { pushMessage } from "../firebase";
+
 const MessageField = ({ name, setText, text }) => {
     const [isComposed, setIsComposed] = useState(false);
 
@@ -13,7 +15,7 @@ const MessageField = ({ name, setText, text }) => {
         const text = e.target.value;
         if(text === '') return;
         if(e.key === 'Enter') {
-          console.log('push message to firebase');
+          pushMessage({ name, text })
           setText('');
           e.preventDefault();
         }
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+apiKey: "AIzaSyArFhDZPuzsCiqoJfnbm1TCyBvolIfBP2A",
+authDomain: "idobatakaigi-with-kag.firebaseapp.com",
+projectId: "idobatakaigi-with-kag",
+storageBucket: "idobatakaigi-with-kag.appspot.com",
+messagingSenderId: "254552960959",
+appId: "1:254552960959:web:6a4ade20533d351ab93b0a"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const messageRef = database.ref('messages');
+
+export const pushMessage = ({ name, text }) => {
+    messageRef.push({ name, text });
+}
