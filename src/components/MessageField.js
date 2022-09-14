1f40ee7f3786465654461dfb61ed3f6ebfb9a3b6
kaguyaweb <h1810518@edu.cc.uec.ac.jp>
2022-09-14 19:25:55 +0900

    adjust focusing

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -3,27 +3,30 @@ import { TextField } from "@material-ui/core";
 
 import { pushMessage } from "../firebase";
 
-const MessageField = ({ name, setText, text }) => {
+const MessageField = ({ inputEl, name, setText, text }) => {
     const [isComposed, setIsComposed] = useState(false);
 
-    return <TextField 
-    fullWidth={true} 
-    onChange={(e) => { setText(e.target.value); }}
-    onKeyDown={(e) => {
-        if(isComposed) return;
-        
-        const text = e.target.value;
-        if(text === '') return;
-        if(e.key === 'Enter') {
-          pushMessage({ name, text })
-          setText('');
-          e.preventDefault();
-        }
-      }}
-    onCompositionStart={() => { setIsComposed(true) }}
-    onCompositionEnd={() => { setIsComposed(false) }}
-    value={text}
-    />;
+    return (
+    <TextField 
+        autoFocus
+        fullWidth={true} 
+        inputRef={inputEl}
+        onChange={(e) => { setText(e.target.value); }}
+        onKeyDown={(e) => {
+            if(isComposed) return;
+            
+            const text = e.target.value;
+            if(text === '') return;
+            if(e.key === 'Enter') {
+            pushMessage({ name, text })
+            setText('');
+            e.preventDefault();
+            }
+        }}
+        onCompositionStart={() => { setIsComposed(true) }}
+        onCompositionEnd={() => { setIsComposed(false) }}
+        value={text}
+    />);
 };
 
 export default MessageField;
